b5f99af2f5dde60b6644a2b46920af65add70523
yvette02 <50646132+yvette02@users.noreply.github.com>
2019-07-30 17:38:48 +0900

    7.30db

---
+++ 7.30hr.sql
@@ -0,0 +1,5 @@
+select *from employees where employee_id = 100;
+UPDATE employees
+set email='chungbuk'
+where employee_id = 100;
+/*자바에서 prepared 메소드를 이용. SetString이 키포인트. */
---
+++ 7.30마당.sql
@@ -0,0 +1,8 @@
+CREATE TABLE members(
+    userid varchar(20) not null,
+    userpwd varchar(20) 
+    
+    
+    );
+    
+    INSERT INTO members VALUES('hong', 'hong');
